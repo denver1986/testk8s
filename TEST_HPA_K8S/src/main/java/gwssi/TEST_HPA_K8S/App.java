00a758c6559d1682ddfc271a89f93a39e9872d34
denver1986 <iamlby@hotmail.com>
2018-05-24 09:41:47 +0800

    TEST

---
+++ TEST_HPA_K8S/src/main/java/gwssi/TEST_HPA_K8S/App.java
@@ -0,0 +1,13 @@
+package gwssi.TEST_HPA_K8S;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
